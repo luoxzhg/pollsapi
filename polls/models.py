d05c773d0fe1f58807b0986fd32f69422437b2e6
Luoxzhg <luoxzhg@gmail.com>
2019-03-19 09:52:48 +0800

    02: create models and register them to admin
    Signed-off-by: Luoxzhg <luoxzhg@gmail.com>

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=100)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, related_name='choices')
+    choice_text = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE, related_name='votes')
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    voted_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("poll", "voted_by")
---
+++ polls/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+# from .apiviews import
+
+urlpatterns = [
+    # url(r'^polls/$', ),
+    # url(r'^polls/(?P<pk>\d+)', )
+]
