5da6f5c38c8a66bec10d3a855d4d64be627a3f8d
Luoxzhg <luoxzhg@gmail.com>
2019-03-19 10:50:11 +0800

    04: create views based on APIView
    Signed-off-by: Luoxzhg <luoxzhg@gmail.com>

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Poll, Choice
+from .serializers import PollSerializer
+
 
 # Create your views here.
+class PollList(APIView):
+    def get(self, request):
+        polls = Poll.objects.all()
+        data = PollSerializer(polls, many=True).data
+        return Response(data=data)
+
+
+class PollDetail(APIView):
+    def get(self, request, pk):
+        poll = get_object_or_404(Poll, pk=pk)
+        data = PollSerializer(poll).data
+        return Response(data=data)
+
--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-# from .apiviews import
+from . import apiviews
 
 urlpatterns = [
-    # url(r'^polls/$', ),
-    # url(r'^polls/(?P<pk>\d+)', )
+    url(r'^polls/$', apiviews.PollList.as_view(), name='polls_list'),
+    url(r'^polls/(?P<pk>\d+)', apiviews.PollDetail.as_view(), name='polls_detail'),
+
 ]
